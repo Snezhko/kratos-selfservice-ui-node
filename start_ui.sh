ccfa8d1e2670aa4eb631b1dd1f7604c80fdfb98f
Eduard Snezhko <eduard.snezhko@gmail.com>
2021-06-14 04:38:25 +0300

    starting UI

--- start_ui.sh
+++ start_ui.sh
@@ -1,8 +1,11 @@
 export SECURITY_MODE=cookie
-export KRATOS_BROWSER_URL=https://127.0.0.1:4433/
-export KRATOS_PUBLIC_URL=https://127.0.0.1:4433/
-export KRATOS_ADMIN_URL=https://127.0.0.1:4434/
+export KRATOS_BROWSER_URL=http://127.0.0.1:4433/
+export KRATOS_PUBLIC_URL=http://127.0.0.1:4433/
+export KRATOS_ADMIN_URL=http://127.0.0.1:4434/
 export PORT=4455
+# export BASE_URL=http://127.0.0.1:4455/
+# export TLS_CERT_PATH='../../testweb/ssl/testweb.crt'
+# export TLS_KEY_PATH='../../testweb/ssl/testweb.key'
 
 echo 'KRATOS_BROWSER_URL='$KRATOS_BROWSER_URL
 echo 'KRATOS_PUBLIC_URL='$KRATOS_PUBLIC_URL
