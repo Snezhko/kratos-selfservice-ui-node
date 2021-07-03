54ea2407d74d261913539c587dc726a8b7ce677f
snezhko <eduard.snezhko@gmail.com>
2021-07-03 03:13:11 +0300

    1. fixed kratos admin address (to fix settings) 2. fixed link to Sign In in registration flow

--- start_ui.sh
+++ start_ui.sh
@@ -1,20 +1,15 @@
 export SECURITY_MODE=cookie
-export KRATOS_BROWSER_URL=https://testweb.org.by/1/
-export KRATOS_PUBLIC_URL=https://testweb.org.by/1/
-export KRATOS_ADMIN_URL=https://testweb.org.by/10/
-export PORT=4455
-export BASE_URL=https://testweb.org.by/0/
-
-export HYDRA_ADMIN_URL=https://testweb.org.by/20/
-
-# export TLS_CERT_PATH='../../testweb/ssl/testweb.crt'
-# export TLS_KEY_PATH='../../testweb/ssl/testweb.key'
-
-export NODE_TLS_REJECT_UNAUTHORIZED='0' # only for dev!!!
+export KRATOS_BROWSER_URL=https://lungs-dev.fr.to/1/
+export KRATOS_PUBLIC_URL=https://lungs-dev.fr.to/1/
+export KRATOS_ADMIN_URL=https://lungs-dev.fr.to/10/
+export PORT=8455
+export BASE_URL=https://lungs-dev.fr.to/0/
 
 echo 'KRATOS_BROWSER_URL='$KRATOS_BROWSER_URL
 echo 'KRATOS_PUBLIC_URL='$KRATOS_PUBLIC_URL
 echo 'KRATOS_ADMIN_URL='$KRATOS_ADMIN_URL
+echo 'BASE_URL='$BASE_URL
+echo 'PORT='$PORT
 
 # npm run serve
 npm start
