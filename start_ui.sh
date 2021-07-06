cd1231c6561844ebf97c43f5b1e3ab2fcb7be1e2
snezhko <eduard.snezhko@gmail.com>
2021-07-06 23:47:15 +0300

    fix

--- start_ui.sh
+++ start_ui.sh
@@ -1,7 +1,7 @@
 export SECURITY_MODE=cookie
 export KRATOS_BROWSER_URL=https://lungs-dev.fr.to/1/
 export KRATOS_PUBLIC_URL=https://lungs-dev.fr.to/1/
-export KRATOS_ADMIN_URL=https://lungs-dev.fr.to/10/
+export KRATOS_ADMIN_URL=https://127.0.0.1:8434/
 export PORT=8455
 export BASE_URL=https://lungs-dev.fr.to/0/
 
