aa12cb6d1fc22f1c73153467bbe22adc0fedd666
snezhko <eduard.snezhko@gmail.com>
2021-07-30 03:45:48 +0300

    updated start_ui.sh - environment variables moved to .env, which used during docker-compose

--- start_ui.sh
+++ start_ui.sh
@@ -1,10 +1,4 @@
 export SECURITY_MODE=cookie
-export KRATOS_BROWSER_URL=https://lungs-dev.fr.to/1/
-export KRATOS_PUBLIC_URL=https://lungs-dev.fr.to/1/
-export KRATOS_ADMIN_URL=https://127.0.0.1:8434/
-export PORT=8455
-export BASE_URL=https://lungs-dev.fr.to/0/
-
 echo 'KRATOS_BROWSER_URL='$KRATOS_BROWSER_URL
 echo 'KRATOS_PUBLIC_URL='$KRATOS_PUBLIC_URL
 echo 'KRATOS_ADMIN_URL='$KRATOS_ADMIN_URL
