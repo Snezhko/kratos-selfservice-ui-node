613541f305eaf83bc9e1c0aaac84bbf8b82892cb
Eduard Snezhko <eduard.snezhko@gmail.com>
2023-03-06 08:34:19 +0000

    Updates for OVH myoweb server

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -49,7 +49,7 @@ export default (
       }
       // removed password elements
       // password is enabled in kratos, temporarily, but should be hidden in UI, until M2M kratos will be enabled
-      flow.ui.nodes.splice(5, 7);
+      flow.ui.nodes.splice(3, 7);
       // Render the data using a view (e.g. Jade Template):
       res.render('login', flow);
     })
